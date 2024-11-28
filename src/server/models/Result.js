a6842e08cc8854c8f09f0aae4c22d8010b019a16
4pple4ree <126749539+4pple4ree@users.noreply.github.com>
2024-11-28 17:11:02 +0900

    Add files via upload

--- src/server/models/Result.js
+++ src/server/models/Result.js
@@ -2,9 +2,13 @@ import mongoose from 'mongoose';
 
 const ResultSchema = new mongoose.Schema({
   timestamp: { type: Date, default: Date.now },
-  classification: String,
-  imagePath: String,
-  weight: Number
+  classification: {
+    type: String,
+    enum: ['trash', 'can', 'glass', 'plastic', 'test data'],
+    required: true
+  },
+  weight: Number,
+  image_id: mongoose.Schema.Types.ObjectId
 });
 
 export default mongoose.model('Result', ResultSchema);
